7c96a506a2e371b75c8b14a29f6764de9040e073
viniciuschagas7 <59289127+viniciuschagas7@users.noreply.github.com>
2021-01-06 07:13:35 -0300

    Add files via upload

--- validador.js
+++ validador.js
@@ -2,11 +2,11 @@ function validarRespostaIndex() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == "relogio" || campoResposta == "Sim" || campoResposta == 'sim' || campoResposta == 'yes' || campoResposta == 'Yes') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/primeira_fase");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/primeira_fase");
         return false;
     } else {
         alert("Sem resposta eu não prossigo.");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/");
         return false;
     }
 }
@@ -15,35 +15,35 @@ function validarRespostaPrimeiraFase() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Páscoa' || campoResposta == 'páscoa' || campoResposta == 'Pascoa' || campoResposta == 'pascoa') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/pascoa");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/pascoa");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/primeira_fase");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/primeira_fase");
         return false;
     }
 }
 
 function recomecar() {
-    window.location.replace("https://viniciuschagas7.github.io/nonameenigma/")
+    window.location.replace("https://viniciuschagas7.github.io/enigmax/")
 }
 
 function validarRespostaPascoa() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'day' || campoResposta == 'Day') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/day");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/day");
         return false;
 
     } else if (campoResposta == 'dia' || campoResposta == 'Dia') {
         alert("A semelhança da semana em português é feira e não day.");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/pascoa");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/pascoa");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/pascoa");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/pascoa");
         return false;
     }
 }
@@ -52,17 +52,17 @@ function validarRespostaDay() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Mercúrio' || campoResposta == 'mercúrio' || campoResposta == 'Mercurio' || campoResposta == 'mercurio') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/mercurio");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/mercurio");
         return false;
 
     } else if (campoResposta == '80' || campoResposta == 'oitenta') {
         alert("De qual elemento?");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/day");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/day");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/day");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/day");
         return false;
     }
 }
@@ -71,11 +71,11 @@ function validarRespostaMercurio() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == "relogio" || campoResposta == "Relogio" || campoResposta == 'relógio' || campoResposta == 'Relógio') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/relogio");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/relogio");
         return false;
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/mercurio");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/mercurio");
         return false;
     }
 }
@@ -84,15 +84,15 @@ function validarRespostaRelogio() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Dr. Facilier' || campoResposta == 'Facilier') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/facilier");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/facilier");
         return false;
     } else if (campoResposta == 'facilier' || campoResposta == 'dr. facilier') {
         alert("Cadê a gramática?");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/relogio");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/relogio");
         return false;
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/relogio");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/relogio");
         return false;
     }
 }
@@ -101,17 +101,17 @@ function validarRespostaFacilier() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Ordep' || campoResposta == 'ordep' || campoResposta == 'ordeP') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/sem_nome");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/sem_nome");
         return false;
 
     } else if (campoResposta == 'Pedro' || campoResposta == 'pedro') {
         alert("Missa Atnet");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/facilier");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/facilier");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/facilier");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/facilier");
         return false;
     }
 }
@@ -120,17 +120,17 @@ function validarRespostaSemNome() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Barriga' || campoResposta == 'barriga') {
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/final");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/final");
         return false;
 
     } else if (campoResposta == 'Toucinho' || campoResposta == 'toucinho' || campoResposta == 'Bacon' || campoResposta == 'bacon') {
         alert("Parabéns! Mas ainda não é a resposta certa.");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/sem_nome");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/sem_nome");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/nonameenigma/sem_nome");
+        window.location.replace("https://viniciuschagas7.github.io/enigmax/sem_nome");
         return false;
     }
 }
