6b49dd1dea0b0aad6f8047849dd7fc19a84eb44f
viniciuschagas7 <59289127+viniciuschagas7@users.noreply.github.com>
2021-01-07 13:58:37 -0300

    Add files via upload

--- validador.js
+++ validador.js
@@ -2,11 +2,11 @@ function validarRespostaIndex() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == "relogio" || campoResposta == "Sim" || campoResposta == 'sim' || campoResposta == 'yes' || campoResposta == 'Yes') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/primeira_fase");
+        window.location.replace("http://bit.ly/2XwZ4aN");
         return false;
     } else {
         alert("Sem resposta eu não prossigo.");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/");
+        window.location.replace("https://enigmax.top/");
         return false;
     }
 }
@@ -15,35 +15,35 @@ function validarRespostaPrimeiraFase() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Páscoa' || campoResposta == 'páscoa' || campoResposta == 'Pascoa' || campoResposta == 'pascoa') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/pascoa");
+        window.location.replace("https://enigmax.top/pascoa");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/primeira_fase");
+        window.location.replace("https://enigmax.top/primeira_fase");
         return false;
     }
 }
 
 function recomecar() {
-    window.location.replace("https://viniciuschagas7.github.io/enigmax/")
+    window.location.replace("https://enigmax.top/")
 }
 
 function validarRespostaPascoa() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'day' || campoResposta == 'Day') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/day");
+        window.location.replace("https://enigmax.top/day");
         return false;
 
     } else if (campoResposta == 'dia' || campoResposta == 'Dia') {
         alert("A semelhança da semana em português é feira e não day.");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/pascoa");
+        window.location.replace("https://enigmax.top/pascoa");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/pascoa");
+        window.location.replace("https://enigmax.top/pascoa");
         return false;
     }
 }
@@ -52,17 +52,17 @@ function validarRespostaDay() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Mercúrio' || campoResposta == 'mercúrio' || campoResposta == 'Mercurio' || campoResposta == 'mercurio') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/mercurio");
+        window.location.replace("https://enigmax.top/mercurio");
         return false;
 
     } else if (campoResposta == '80' || campoResposta == 'oitenta') {
         alert("De qual elemento?");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/day");
+        window.location.replace("https://enigmax.top/day");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/day");
+        window.location.replace("https://enigmax.top/day");
         return false;
     }
 }
@@ -71,11 +71,11 @@ function validarRespostaMercurio() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == "relogio" || campoResposta == "Relogio" || campoResposta == 'relógio' || campoResposta == 'Relógio') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/relogio");
+        window.location.replace("https://enigmax.top/relogio");
         return false;
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/mercurio");
+        window.location.replace("https://enigmax.top/mercurio");
         return false;
     }
 }
@@ -84,15 +84,15 @@ function validarRespostaRelogio() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Dr. Facilier' || campoResposta == 'Facilier') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/facilier");
+        window.location.replace("https://enigmax.top/facilier");
         return false;
     } else if (campoResposta == 'facilier' || campoResposta == 'dr. facilier') {
         alert("Cadê a gramática?");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/relogio");
+        window.location.replace("https://enigmax.top/relogio");
         return false;
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/relogio");
+        window.location.replace("https://enigmax.top/relogio");
         return false;
     }
 }
@@ -101,17 +101,17 @@ function validarRespostaFacilier() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Ordep' || campoResposta == 'ordep' || campoResposta == 'ordeP') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/sem_nome");
+        window.location.replace("https://enigmax.top/sem_nome");
         return false;
 
     } else if (campoResposta == 'Pedro' || campoResposta == 'pedro') {
         alert("Missa Atnet");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/facilier");
+        window.location.replace("https://enigmax.top/facilier");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/facilier");
+        window.location.replace("https://enigmax.top/facilier");
         return false;
     }
 }
@@ -120,17 +120,17 @@ function validarRespostaSemNome() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Barriga' || campoResposta == 'barriga') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/barriga");
+        window.location.replace("https://enigmax.top/barriga");
         return false;
 
     } else if (campoResposta == 'Toucinho' || campoResposta == 'toucinho' || campoResposta == 'Bacon' || campoResposta == 'bacon') {
         alert("Parabéns! Mas ainda não é a resposta certa.");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/sem_nome");
+        window.location.replace("https://enigmax.top/sem_nome");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/sem_nome");
+        window.location.replace("https://enigmax.top/sem_nome");
         return false;
     }
 }
@@ -139,17 +139,17 @@ function validarRespostaBarriga() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == 'Pedra Filosofal' || campoResposta == 'pedra filosofal') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/pedra_filosofal");
+        window.location.replace("https://enigmax.top/pedra_filosofal");
         return false;
 
     } else if (campoResposta == 'nicolas flamel' || campoResposta == 'Nicolas Flamel') {
         alert("Está perto! ✡");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/barriga");
+        window.location.replace("https://enigmax.top/barriga");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/barriga");
+        window.location.replace("https://enigmax.top/barriga");
         return false;
     }
 }
@@ -158,17 +158,17 @@ function validarRespostaPedraFilosofal() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == '119') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/119");
+        window.location.replace("https://enigmax.top/119");
         return false;
 
     } else if (campoResposta == '129') {
         alert("Reflita, responda!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/pedra_filosofal");
+        window.location.replace("https://enigmax.top/pedra_filosofal");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/pedra_filosofal");
+        window.location.replace("https://enigmax.top/pedra_filosofal");
         return false;
     }
 }
@@ -177,17 +177,17 @@ function validarResposta119() {
     // OLHAR AQUI É TRAPACEAR E NÃO TEM GRAÇA PARA O JOGO. VOCÊ É UM(A) PERDEDOR(A)!
     var campoResposta = document.forms["respostaForm"]["resposta"].value;
     if (campoResposta == '1962') {
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/final");
+        window.location.replace("http://bit.ly/35g3V43");
         return false;
 
     } else if (campoResposta == 'alcatraz' || campoResposta == 'ilha de alcatraz') {
         alert("(x,y)");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/119");
+        window.location.replace("https://enigmax.top/119");
         return false;
 
     } else {
         alert("Resposta errada!");
-        window.location.replace("https://viniciuschagas7.github.io/enigmax/119");
+        window.location.replace("https://enigmax.top/119");
         return false;
     }
 }
